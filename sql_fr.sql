dcf79e676c463b4b72a7b18f6ead735168afe50f
WirzRexTon <sacha.nollot@lyceeclosmaire.onmicrosoft.com>
2023-08-21 20:59:10 +0200

    Fix

--- sql_fr.sql
+++ sql_fr.sql
@@ -14,11 +14,11 @@ INSERT INTO `jobs`(`name`, `label`, `whitelisted`) VALUES
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('vigne', 0, 'recrue', 'Recrue', 12, '{}', '{}'),
+	('vigne', 0, 'rookie', 'Recrue', 12, '{}', '{}'),
 	('vigne', 1, 'novice', 'Novice', 24, '{}', '{}'),
-	('vigne', 2, 'expérimenté', 'Expérimenté', 36, '{}', '{}'),
-	('vigne', 3, 'chef', "Chef d\'équipe", 48, '{}', '{}'),
-	('vigne', 4, 'patron', 'Patron', 0, '{}', '{}')
+	('vigne', 2, 'experienced', 'Expérimenté', 36, '{}', '{}'),
+	('vigne', 3, 'chief', "Chef d\'équipe", 48, '{}', '{}'),
+	('vigne', 4, 'boss', 'Patron', 0, '{}', '{}')
 ;
 
 -- Check README for items
